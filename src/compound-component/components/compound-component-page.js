8ba2614de8c6f78c1a10c5299e67907b7d3a4e3b
Leonardo Omaña <leotangram@gmail.com>
2021-05-28 15:19:19 -0500

    Composite component

--- src/compound-component/components/compound-component-page.js
+++ src/compound-component/components/compound-component-page.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {FinalTodo, TodoForm, TodoList, TodoTitle} from './final-todo';
+import {Todo} from './normal-todo';
 
 export const CompoundComponentPage = () => (
   <>
-    <h2>Compound Component</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
+    <h2>Ejemplo sin Compound Component</h2>
+    <Todo title="Sin Compound Pattern :o" />
+    <hr />
+
+    <h2>Ejemplo con Compound Component</h2>
+    <FinalTodo>
+      <TodoTitle>
+        <h1>Compound Pattern :D</h1>
+      </TodoTitle>
+      <TodoForm />
+      <TodoList />
+    </FinalTodo>
+
+    <h2>Ejemplo Variente con Compound Component</h2>
+    <FinalTodo>
+      <FinalTodo.Title>
+        <h1>Compound Pattern :D</h1>
+      </FinalTodo.Title>
+      <FinalTodo.Form />
+      <FinalTodo.List />
+    </FinalTodo>
   </>
 );
