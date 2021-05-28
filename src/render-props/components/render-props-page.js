05980d466c72f545c9663a96e6634d8cea4abf13
Leonardo Omaña <leotangram@gmail.com>
2021-05-28 15:31:17 -0500

    Render props

--- src/render-props/components/render-props-page.js
+++ src/render-props/components/render-props-page.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {ErrorBoundary} from './error-boundary';
+import {FinalErrorBoundary} from './final-error-boundary';
+
+const MyBug = () => {
+  const [isError, setIsError] = useState(false);
+
+  const handleCrash = () => {
+    setIsError(true);
+  };
+
+  if (isError) {
+    throw new Error('nani?');
+  }
+
+  return <button onClick={handleCrash}>Crashear la app</button>;
+};
 
 export const RenderPropsPage = () => (
   <>
-    <h2>Render Props</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
+    <h1>Render Props</h1>
+    <h2>Ejemplo sin Render Props</h2>
+    <ErrorBoundary>
+      <MyBug />
+    </ErrorBoundary>
+    <hr />
+    <h2>Ejemplo con Render Props</h2>
+    <FinalErrorBoundary render={error => <p>{`Ups D: ${error.message}`}</p>}>
+      <MyBug />
+    </FinalErrorBoundary>
   </>
 );
